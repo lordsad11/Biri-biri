408030d184970f061225c3ffe2a1baf6856ad607
lordsad11 <124650934+lordsad11@users.noreply.github.com>
2024-05-19 21:42:28 +0700

    [•]

--- Auput/plugins/misc/autoleave.py
+++ Auput/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOG_GROUP_ID
-                                and i.chat.id != -1001608701614
-                                and i.chat.id != -1001629504293
+                                and i.chat.id != -1002054986459
+                                and i.chat.id != -1001670334318
                             ):
                                 if left == 20:
                                     continue
