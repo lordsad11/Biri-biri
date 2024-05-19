4c3cbc1666024cc7e6e027e4b97e22fb3911c279
lordsad11 <124650934+lordsad11@users.noreply.github.com>
2024-05-19 21:57:21 +0700

    [•]

--- Auput/plugins/tools/welcome.py
+++ Auput/plugins/tools/welcome.py
@@ -15,11 +15,8 @@ LOGGER = getLogger(__name__)
 
 
 uputlogo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://graph.org/file/5317bd3fc2281f9c29a33.jpg",
+    "https://graph.org/file/61017d3309818d69e8c49.jpg",
 ]
 
 
